6709cfd31ed685e6f1cc76c87ea74a13d33ed868
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2022-11-21 16:59:14 +0100

    Show tasks with a defined array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+
+  const [tasks, setTasks]=useState([
+    {
+      id:1,
+      text: 'Doctors appointment',
+      day: 'Feb 5, 8:00',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'Buy eggs',
+      day: 'Feb 5, 8:00',
+      reminder: true,
+    }
+
+  ])
+
   return (
     <div className="container">
-    <Header></Header>
+    <Header/>
+    <Tasks tasks={tasks}/>
     </div>
   );
 }
