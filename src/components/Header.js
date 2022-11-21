f6edfd69ed6f5c68927d9ff6ea30b607fca1ce17
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2022-11-21 15:42:50 +0100

    Button props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
+
 import React from 'react'
 
 const Header = ({title}) => {
+
+    const onClick=()=>{
+        console.log("click")
+    }
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <button className='btn'>Add</button>
+       <Button color="green" text="Add" onClick={onClick}/>
     </header> 
   )
 }
