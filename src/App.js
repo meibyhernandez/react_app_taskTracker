0b2d02d12e99caa27afe44675caec0b87c7963aa
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2022-11-22 08:45:48 +0100

    onToggle reminder for tasks

--- src/App.js
+++ src/App.js
@@ -25,10 +25,15 @@ function App() {
     setTasks(tasks.filter((task)=>task.id !==id))
   }
 
+  //Toggle reminder
+  const toogleReminder = (id) =>  {
+    setTasks(tasks.map((task)=>task.id===id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
     <Header/>
-    {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask}/>: 'No tasks to show '}
+    {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toogleReminder}/>: 'No tasks to show '}
     </div>
   );
 }
