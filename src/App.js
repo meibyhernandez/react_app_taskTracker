57682d537ebfdc9969c37e768b0a8296e48190b2
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2022-11-24 10:42:15 +0100

    Toogle add task form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
-function App() {
+function App() {  
+
+  const [showAddTask, setShowAddTask] = useState(false) 
 
   const [tasks, setTasks]=useState([
     {
@@ -41,8 +43,9 @@ function App() {
 
   return (
     <div className="container">
-    <Header/>
-    <AddTask onAdd={addTask}/>
+    <Header onAdd={() => setShowAddTask(!showAddTask)} 
+    showAdd={showAddTask}/>
+    {showAddTask && <AddTask onAdd={addTask}/>}
     {tasks.length>0 ? <Tasks tasks={tasks} 
     onDelete={deleteTask} 
     onToggle={toogleReminder}/>: 'No tasks to show '}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,14 @@ import Button from './Button'
 
 import React from 'react'
 
-const Header = ({title}) => {
-
-    const onClick=()=>{
-        console.log("click")
-    }
-
+const Header = ({title, onAdd, showAdd}) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-       <Button color="green" text="Add" onClick={onClick}/>
+       <Button 
+       color={showAdd ? 'red': 'green'} 
+       text={showAdd ? 'Close' : 'Add'} 
+       onClick={onAdd}/>
     </header> 
   )
 }
